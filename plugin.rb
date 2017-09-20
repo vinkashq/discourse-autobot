28ec52727c5f19f39d9c7e07edbc16f9101b6c32
Vinoth Kannan <vinothkannan@vinkas.com>
2017-09-20 19:22:36 +0530

    RENAME: 'discourse-autobot' to 'autobot'

--- plugin.rb
+++ plugin.rb
@@ -1,20 +1,20 @@
-# name: discourse-autobot
+# name: autobot
 # about: Automatic content creator bot for Discourse
 # version: 0.0.1
 # authors: Vinoth Kannan (@vinothkannans)
-# url: https://github.com/vinkashq/autobot
+# url: https://github.com/vinkashq/discourse-autobot
 
 enabled_site_setting :discourse_autobot_enabled
 
 after_initialize do
-  SeedFu.fixture_paths << Rails.root.join("plugins", "discourse-autobot", "db", "fixtures").to_s
+  SeedFu.fixture_paths << Rails.root.join("plugins", "autobot", "db", "fixtures").to_s
 
-  module ::DiscourseAutobot
-    PLUGIN_NAME = "discourse-autobot".freeze
+  module ::Autobot
+    PLUGIN_NAME = "autobot".freeze
 
     class Engine < ::Rails::Engine
       engine_name PLUGIN_NAME
-      isolate_namespace DiscourseAutobot
+      isolate_namespace Autobot
     end
 
     class Store
